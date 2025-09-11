61c373bcdba47319dc9026d03510239144fe2ddb
CalebL42 <caleb_lee@ucsb.edu>
2025-09-11 02:32:49 -0700

    puppeteer + cheerio webscraper is working,, se setting up node server now

---
+++ scrape.js
@@ -0,0 +1,57 @@
+import puppeteerCore from 'puppeteer';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { addExtra } from 'puppeteer-extra';
+import * as cheerio from 'cheerio';
+
+//Searches up "searchItem" on amazon. Returns a list of product data objects from the search results. 
+async function getProductData(searchItem) {
+  //set up browser environment with stealthy mode...
+  const puppeteer = addExtra(puppeteerCore);
+  puppeteer.use(StealthPlugin());
+  const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36';
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  const page = await browser.newPage();
+  await page.setUserAgent(userAgent);
+  await page.setViewport({ width: 1280, height: 720 });
+  await page.goto('https://www.amazon.com', {
+    waitUntil: 'networkidle2',
+  });
+  //navigate to the desired page, and extract relevant html
+  await page.locator(`input[type='text']`).fill(`${searchItem}\n`);
+  await page.waitForNavigation();
+  const body = await page.content();
+  
+  let productList = [];
+  const $ = cheerio.load(body);
+  $(`div[role="listitem"]`).each((index, element) => {
+    //only grab 50 products
+    if (index >= 50) return;
+
+    let currentProduct = {
+      image: '',
+      title: '',
+      price: '',
+      stars: '',
+      total_reviews: '',
+      url: ''
+    };
+
+    console.log(`product #${index}:`)
+    currentProduct['image'] = $(element).find(`img`).attr(`src`);
+    currentProduct['title'] = $(element).find(`h2 > span`).text();
+    currentProduct['price'] = $(element).find(`span:has(> span.a-price-symbol)`).text();
+    currentProduct['stars'] = $(element).find(`i[data-cy="reviews-ratings-slot"]`).text().split(' ')[0];
+    currentProduct['total_reviews'] = $(element).find(`span.rush-component > div > a > span`).text();
+    //console.log(JSON.stringify(currentProduct));
+
+    productList.push(currentProduct);
+  });
+  
+  await browser.close();
+  return productList;
+}
+
+export default getProductData;
