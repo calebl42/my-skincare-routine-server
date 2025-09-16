8057f365ab6af7a546d23d6b70ac919d534fb028
Ubuntu <ubuntu@ip-172-31-23-247.ec2.internal>
2025-09-16 10:16:16 +0000

    fixed double JSON.stringify

--- server.js
+++ server.js
@@ -2,7 +2,25 @@ import express from 'express';
 import getScrapedData from './scrape.js';
 import { connectDB, getDB } from './db/connect.js';
 import * as queries from './db/queries.js';
+import cors from 'cors';
 const app = express();
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://my-skincare-routine.vercel.app/'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 app.get('/products', async (req, res) => {
   const productName = req.query.productName;
@@ -14,17 +32,16 @@ app.get('/products', async (req, res) => {
   if (!curProductList) {
     //no database entry exists
     const newProductList = await getScrapedData(productName);
-    res.json(JSON.stringify(newProductList));
     await queries.insertProductList(db, newProductList);
   } else if (curDate - new Date(curProductList.date_created).getTime() > 86400) {
     /*database entry for this product has not been updated in over a day
     therefore we need to update the db after returning the old productList*/
-    res.json(JSON.stringify(curProductList));
+    res.json(curProductList);
     const newProductList = await getScrapedData(productName);
     await queries.updateProductList(db, productName, newProductList);
   } else {
     //database entry exists and is up to date
-    res.json(JSON.stringify(curProductList));
+    res.json(curProductList);
   }
 });
 
