f35dfbbf039d08f7933deb5558cd67d968c61e47
CalebL42 <caleb_lee@ucsb.edu>
2025-10-01 23:17:23 -0700

    trying to get the webscraping logic up and running again on eb

--- server.js
+++ server.js
@@ -37,22 +37,21 @@ app.get('/products', async (req, res) => {
   
   const curProductList = await queries.getProductList(db, productName);
 
-  // if (!curProductList) {
-  //   //no database entry exists
-  //   const newProductList = await getScrapedData(productName);
-  //   res.json(newProductList);
-  //   await queries.insertProductList(db, newProductList);
-  // } else if (curDate - new Date(curProductList.date_created).getTime() > 86400000) {
-  //   /*database entry for this product has not been updated in over a day
-  //   therefore we need to update the db after returning the old productList*/
-  //   res.json(curProductList);
-  //   const newProductList = await getScrapedData(productName);
-  //   await queries.updateProductList(db, productName, newProductList);
-  // } else {
-  //   //database entry exists and is up to date
-  //   res.json(curProductList);
-  // }
-  res.json(curProductList);
+  if (!curProductList) {
+    //no database entry exists
+    const newProductList = await getScrapedData(productName);
+    res.json(newProductList);
+    await queries.insertProductList(db, newProductList);
+  } else if (curDate - new Date(curProductList.date_created).getTime() > 3 * 86400000) {
+    /*database entry for this product has not been updated in over 3 days
+    therefore we need to update the db after returning the old productList*/
+    res.json(curProductList);
+    const newProductList = await getScrapedData(productName);
+    await queries.updateProductList(db, productName, newProductList);
+  } else {
+    //database entry exists and is up to date
+    res.json(curProductList);
+  }
 });
 
 //connect to MongoDB first, then start up server
